ef7c92c0a55d080736b6fc34b43ed7f8b175a4a0
Roshan <roshan.jha@hih7.in>
2023-10-06 12:10:20 +0530

    implement css in blog frontend project

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main>
+        <header>
+          <a href="" className="logo">
+            MyBlog
+          </a>
+          <nav>
+            <a href="">Login</a>
+            <a href="">Register</a>
+          </nav>
+        </header>
+        <div className="post">
+          <div className="image">
+            <img src="https://media.istockphoto.com/id/185568132/photo/outdoor-shot-of-garden-equipment.jpg?s=612x612&w=0&k=20&c=lbVrk5n0QKttg3o-dObaBRHAPOXdaFCr4W8zFOnSt-g=" />
+          </div>
+          <div className="texts">
+            <h2>Full house battery backup coming later this year</h2>
+            <p className="info">
+              <a className="author">Roshan Kumar Jha</a>
+              <time>2023-01-06 16:45</time>
+            </p>
+            <p className="summary">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi id
+              enim pariatur, alias ullam animi corrupti nulla! Magni illo rerum
+              doloribus, libero enim nulla mollitia, velit delectus id commodi
+              excepturi quam, laborum fugiat dolor aliquid nam ipsam asperiores
+              eius dolorem ullam vel numquam officia? Quibusdam asperiores velit
+              corrupti quaerat ad?
+            </p>
+          </div>
+        </div>
+        <div className="post">
+          <div className="image">
+            <img src="https://media.istockphoto.com/id/185568132/photo/outdoor-shot-of-garden-equipment.jpg?s=612x612&w=0&k=20&c=lbVrk5n0QKttg3o-dObaBRHAPOXdaFCr4W8zFOnSt-g=" />
+          </div>
+          <div className="texts">
+            <h2>Full house battery backup coming later this year</h2>
+            <p className="info">
+              <a className="author">Roshan Kumar Jha</a>
+              <time>2023-01-06 16:45</time>
+            </p>
+            <p className="summary">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi id
+              enim pariatur, alias ullam animi corrupti nulla! Magni illo rerum
+              doloribus, libero enim nulla mollitia, velit delectus id commodi
+              excepturi quam, laborum fugiat dolor aliquid nam ipsam asperiores
+              eius dolorem ullam vel numquam officia? Quibusdam asperiores velit
+              corrupti quaerat ad?
+            </p>
+          </div>
+        </div>
+        <div className="post">
+          <div className="image">
+            <img src="https://media.istockphoto.com/id/185568132/photo/outdoor-shot-of-garden-equipment.jpg?s=612x612&w=0&k=20&c=lbVrk5n0QKttg3o-dObaBRHAPOXdaFCr4W8zFOnSt-g=" />
+          </div>
+          <div className="texts">
+            <h2>Full house battery backup coming later this year</h2>
+            <p className="info">
+              <a className="author">Roshan Kumar Jha</a>
+              <time>2023-01-06 16:45</time>
+            </p>
+            <p className="summary">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi id
+              enim pariatur, alias ullam animi corrupti nulla! Magni illo rerum
+              doloribus, libero enim nulla mollitia, velit delectus id commodi
+              excepturi quam, laborum fugiat dolor aliquid nam ipsam asperiores
+              eius dolorem ullam vel numquam officia? Quibusdam asperiores velit
+              corrupti quaerat ad?
+            </p>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
