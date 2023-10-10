d7b0f0ca6fba19b2dbe9fe70610765de63747131
Roshan <roshan.jha@hih7.in>
2023-10-10 10:00:11 +0530

    register api is added

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 const RegisterPage = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const register = async (ev) => {
+    ev.preventDefault();
+    console.log(username, email, password);
+    const result = await fetch("http://localhost:8000/user/register", {
+      method: "POST",
+      body: JSON.stringify({ username, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+  };
   return (
-    <form className="register">
+    <form className="register" onSubmit={register}>
       <h1>REGISTER</h1>
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
-      <button>Register</button>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(ev) => setUsername(ev.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="type your mailid"
+        value={email}
+        onChange={(ev) => setEmail(ev.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(ev) => setPassword(ev.target.value)}
+      />
+      <button> Register</button>
     </form>
   );
 };
